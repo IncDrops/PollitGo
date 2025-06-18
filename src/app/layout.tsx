ea2ef1c554bbcdff8dd9719c4426920a7ade1c67
Harley Knox <therealharleyknox@gmail.com>
2025-06-18 22:55:11 +0000

    "modified:   src/app/layout.tsx"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=League+Spartan:wght@400;600;700&family=Alice&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/pollit.png" sizes="40x40" type="image/png" />
       </head>
       <body className="font-body antialiased h-full flex flex-col bg-background">
         <ThemeProvider
